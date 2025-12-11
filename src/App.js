fe0866bc9c8262831e69320ceeee83af5a379e79
Korotchenko Igor <korotchenkoigor21@gmail.com>
2025-12-11 13:38:15 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     return (
         <div className="App">
             <PostItem />
+            <PostItem />
+            <PostItem />
+            <PostItem />
+            <PostItem />
         </div>
     );
 }
